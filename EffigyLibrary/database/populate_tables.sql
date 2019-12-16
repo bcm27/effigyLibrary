a7adaacb017bc85b2f8cf3c304771b7f7930f803
Bjorn <bcm27@live.com>
2019-12-16 12:17:07 -0600

    fresh start with graphql-dotnet

--- EffigyLibrary/database/populate_tables.sql
+++
@@ -1,5 +0,0 @@
-use effigylibrarytest;
-
-insert into Account(userName, firstName, lastName, middleIntitial, email, phoneNumb, address, state, zipCode) value 
-('bcm27', 'Bjørn', 'Mathisen', 'C', 'thebcm27@gmail.com', '224-622-8543', '540 Lilac Drive North', 'MN', '55422'),
-('bladeduchess','Alora', 'Cruz', 'J');
