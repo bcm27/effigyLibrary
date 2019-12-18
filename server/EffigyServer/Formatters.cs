b86b86bbf275ef9f86a3c057ad396132e6b9c61e
bcm27 <bcm27@Live.com>
2019-12-18 00:54:27 -0600

    Reconfigured file to work on core 3.1

---
+++ server/EffigyServer/Formatters.cs
@@ -0,0 +1,6 @@
+namespace Formatters {    
+    public class Formatters {
+        public static string _10Stars = "**********";
+        public static string _20Stars = "********************";
+    }
+}
