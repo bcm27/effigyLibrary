5fc86b45a27833c985f226c068e33ca331e6445e
bcm27 <bcm27@Live.com>
2019-12-19 21:00:02 -0600

    Removing old GraphQL to make room for Hot Chocolate.
    Its more feature rich than the normal GraphQL. As such we need a fresh
    project to not have overlapping redundencies.
    
    More about hot chocolate can be found at this url:
    https://hotchocolate.io/docs/introduction.html

--- server/EffigyServer/Formatters.cs
+++
@@ -1,6 +0,0 @@
-namespace Formatters {    
-    public class Formatters {
-        public static string _10Stars = "**********";
-        public static string _20Stars = "********************";
-    }
-}
