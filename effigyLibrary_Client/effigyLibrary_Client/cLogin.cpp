ad562f0a6556abd32f6ee71a00ca25a3f49b1700
bcm27 <bcm27@Live.com>
2020-01-02 21:23:55 -0600

    Added a static message for user list

--- effigyLibrary_Client/effigyLibrary_Client/cLogin.cpp
+++ effigyLibrary_Client/effigyLibrary_Client/cLogin.cpp
@@ -1,13 +1,13 @@
 #include "cLogin.h"
 
-cLogin::cLogin() : wxFrame(nullptr, wxID_HOME, "Effigy Library - Login", wxPoint(), wxSize(400, 225))
+cLogin::cLogin() : wxFrame(nullptr, wxID_HOME, "Effigy Library - Login", wxPoint(), wxSize(385, 225))
 {
-	cLogin_btn_login = new wxButton(this, wxID_ANY, "Login", wxPoint(75,135), wxSize(100,30));
-	cLogin_txt_userName = new wxTextCtrl(this, wxID_ANY, "Username", wxPoint(75,100), wxSize(100,20));
-	cLogin_btn_register = new wxButton(this, wxID_ANY, "Register", wxPoint(200, 135), wxSize(100, 30));
-	cLogin_txt_password = new wxTextCtrl(this, wxID_ANY, "Password", wxPoint(200, 100), wxSize(100, 20));
-
-	cLogin_txt_storedUsers = new wxListBox(this, wxID_ANY, wxPoint(10,10), wxSize(50,165));
+	cLogin_btn_login = new wxButton(this, wxID_ANY, "Login", wxPoint(110,135), wxSize(100,30));
+	cLogin_txt_userName = new wxTextCtrl(this, wxID_ANY, "Username", wxPoint(110,100), wxSize(100,20));
+	cLogin_btn_register = new wxButton(this, wxID_ANY, "Register", wxPoint(250, 135), wxSize(100, 30));
+	cLogin_txt_password = new wxTextCtrl(this, wxID_ANY, "Password", wxPoint(250, 100), wxSize(100, 20));
+	cLogin_txt_storedUsers = new wxListBox(this, wxID_ANY, wxPoint(10,10), wxSize(80,165));
+	cLogin_sttxt_storedUsers = new wxStaticText(this, wxID_ANY, "", wxPoint(), wxSize());
 }
 
 cLogin::~cLogin()
