fc095545b582965ac4d6d268e5089a2b7ec0fff8
bcm27 <bcm27@Live.com>
2020-01-05 19:17:44 -0600

    Added barebones server project

---
+++ effigyLibrary_Server/effigyLibrary_Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace effigyLibrary_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
