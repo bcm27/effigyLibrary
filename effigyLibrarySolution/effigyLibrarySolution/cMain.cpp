d2d27b9c5b4942f833b84f839063ed5191c38e45
bcm27 <bcm27@Live.com>
2020-01-08 19:39:10 -0600

    renaming solution by copying files, and adding alternative projects to solution for code integration

---
+++ effigyLibrarySolution/effigyLibrarySolution/cMain.cpp
@@ -0,0 +1,10 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_HOME, "Effigy Library - Library", wxPoint(), wxSize(800, 600))
+{
+
+}
+
+cMain::~cMain()
+{
+}
---
+++ effigyLibrarySolution/effigyLibrarySolution/cMain.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "wx/wx.h"
+#include <wx/frame.h>
+
+class cMain : public wxFrame {
+public:
+	cMain();
+	~cMain();
+
+};
