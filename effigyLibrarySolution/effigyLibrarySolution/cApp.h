797d9967e690bb233fce958f45e477ee124c1544
bcm27 <bcm27@Live.com>
2020-01-08 19:39:10 -0600

    renaming solution by copying files, and adding alternative projects to solution for code integration

---
+++ effigyLibrarySolution/effigyLibrarySolution/cApp.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "cMain.h"
+#include "cLogin.h"
+#include "wx/wx.h"
+
+class cApp : public wxApp {
+public:
+	cApp();
+	~cApp();
+
+public:
+	virtual bool OnInit();
+
+private:
+
+	cLogin* m_frame01 = nullptr;
+	cMain* m_frame02 = nullptr;
+};
