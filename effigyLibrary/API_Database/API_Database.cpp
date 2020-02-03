1be357e28a2a254b8526451e10f5ceb9a613510a
Bjorn <bcm27@live.com>
2020-02-02 21:37:15 -0600

    Added database access methods for user and added a struct

--- effigyLibrary/API_Database/API_Database.cpp
+++ effigyLibrary/API_Database/API_Database.cpp
@@ -1,7 +1,15 @@
 #include "API_Database.h"
-#include "C:\skds\plog-master\include\plog\log.h"
+// #include "C:\skds\plog-master\include\plog\log.h"
 
 void API_Database::printStatus()
 {
-	PLOG_DEBUG << "We have accesed the database file";
+	//PLOG_DEBUG << "We have accesed the database file";
+}
+
+void API_Database::getUserInfo(userInfo user) {
+	userInfo retUserInfo;
+	retUserInfo = NULL;
+
+
+	return retUserInfo;
 }
--- effigyLibrary/API_Database/API_Database.h
+++ effigyLibrary/API_Database/API_Database.h
@@ -4,9 +4,19 @@
 
 #include <iostream>
 #include <string>
+#include <string>
 
 namespace API_Database {
+
+	struct userInfo {
+		std::string userName;
+		std::string firstName;
+		std::string lastName; 
+		std::string tempPassword;
+	};
+
 	void printStatus();
+	void getUserInfo(userInfo user);
 }
 
 #endif // !_EFFIGY_API_DATABASE_H
