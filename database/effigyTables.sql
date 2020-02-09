b9b8c244c82e0291b961c187f3bf14a9dcb90501
Bjorn <Bcm27@Live.com>
2020-02-08 21:02:26 -0600

    working table with insert statements

--- database/effigyTables.sql
+++ database/effigyTables.sql
@@ -1,21 +1,20 @@
-drop database if exists effigy_users;
-create database effigy_users;
-use effigy_users;
+CREATE DATABASE EFFIGY;
+USE EFFIGY;
 
-/* schema */
-
-CREATE TABLE 'Account' (
-	'id' INT(11) PRIMARY KEY AUTO_INCREMENT,
-	'userName' VARCHAR(30) UNIQUE NOT NULL,
-	'firstName' VARCHAR(30) NOT NULL,
-	'middleInitial' VARCHAR(30) default null,
-	'lastName' VARCHAR(30) NOT NULL,
-	'email' VARCHAR(30) UNIQUE NOT NULL,
-	'phoneNumber' VARCHAR(120) DEFAULT NULL,
-	'address' VARCHAR(120) DEFAULT NULL,
-	'state' CHAR(2) DEFAULT NULL,
-	'zipcode' INT(6) DEFAULT NULL,
-	'passwordHash' CHAR(64) NOT NULL,
-	'salt' CHAR(12) NOT NULL
+CREATE TABLE `users` (
+	`id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+	`userName` VARCHAR(30) DEFAULT NULL,
+	`firstName` VARCHAR(30) NOT NULL,
+	`middleInitial` VARCHAR(30) default null,
+	`lastName` VARCHAR(30) NOT NULL,
+	`email` VARCHAR(30) UNIQUE NOT NULL,
+	`phoneNumber` VARCHAR(120) DEFAULT NULL,
+	`address` VARCHAR(120) DEFAULT NULL,
+	`state` CHAR(2) DEFAULT NULL,
+	`zipcode` INT(6) DEFAULT NULL,
+	`passwordHash` CHAR(64) NOT NULL,
+	`salt` CHAR(12) NOT NULL
 );
 
+insert into `users` (id, userName, firstName, middleInitial, lastName, email, phoneNumber, address, state, zipcode, passwordHash, salt)
+values (1,'bcm27','bjorn','c','mathisen','bcm27@Live.com','224','540 lilac','mn','55422','hash','salt');
